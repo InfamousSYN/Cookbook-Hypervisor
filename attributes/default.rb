7aa8b21147d5678539f133b0c94eb743d9ac318b
InfamousSYN <ainfamoussyn@gmail.com>
2022-05-25 22:51:36 +1000

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,8 @@ default[:general][:vmware][:enable] = true
 default[:general][:dependencies] = ['libaio1', 'gcc', 'xserver-xorg-core', 'build-essential']
 
 # Virtualbox
-default[:virtualbox][:dependencies] = ['virtualbox', 'virtualbox-dkms', 'virtualbox-guest-additions-iso', 'virtualbox-guest-x11', 'virtualbox-ext-pack']
+default[:virtualbox][:dependencies] = ['virtualbox', 'virtualbox-dkms', 'virtualbox-guest-additions-iso', 'virtualbox-guest-x11']
+default[:virtualbox[:url] = 'https://download.virtualbox.org/virtualbox'
 
 # Vmware
 default[:vmware][:dependencies] = []
